ecb80759b9c6e4c885985315def822fed004d9b1
Markku Rontu <markku.rontu@iki.fi>
2012-11-22 22:44:16 +0200

    Prepare release 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject crt "1.0.0-SNAPSHOT"
+(defproject crt "0.1.0"
   :description "CRT - Clojure Refactoring Tool"
+  :url "https://github.com/Macroz/crt"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}  
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.tinyjee.jgraphx/jgraphx "1.10.3.2"]]
-  )
+                 [org.tinyjee.jgraphx/jgraphx "1.10.3.2"]])
